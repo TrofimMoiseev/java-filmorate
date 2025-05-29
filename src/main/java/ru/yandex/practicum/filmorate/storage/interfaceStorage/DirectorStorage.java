d3f78458dcd0bc45b270d7122e6663fd72f69e20
Tumar Amirzhan <amiramutt@gmail.com>
2025-05-30 01:26:30 +0500

    Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaceStorage/DirectorStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaceStorage/DirectorStorage.java
@@ -7,10 +7,16 @@ import java.util.Optional;
 
 public interface DirectorStorage {
     Optional<Director> findDirectorById(Long id);
+
     Collection<Director> findAll();
+
     Director create(Director director);
+
     Director update(Director director);
+
     boolean checkId(Long id);
+
     void deleteDirector(Long id);
+
     void deleteDirectorFromFilm(Long directorId, Long filmId);
 }
