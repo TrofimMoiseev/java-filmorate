57e2d9f4d3bdcebeba4b40095502571c0ed79244
daniyardoskulov <daniyar.doskulov@narxoz.kz>
2025-05-31 00:38:35 +0500

    "fix"

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -24,6 +24,7 @@ public class ReviewService {
 
     public Review create(Review review) {
         validateReview(review);
+        review.setUseful(0);
         return reviewStorage.create(review);
     }
 
