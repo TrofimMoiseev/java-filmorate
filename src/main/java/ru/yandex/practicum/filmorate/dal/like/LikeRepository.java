740dbf715069e7b7d58df1f7ec96a760df80c8b8
daniyardoskulov <daniyar.doskulov@narxoz.kz>
2025-06-02 00:50:32 +0500

    remove in LikeRepository findpopular

--- src/main/java/ru/yandex/practicum/filmorate/dal/like/LikeRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/like/LikeRepository.java
@@ -17,9 +17,6 @@ import java.util.Collection;
 public class LikeRepository extends BaseRepository<Like> {
     private static final String INSERT_QUERY = "INSERT INTO likes(user_id, film_id) VALUES (?, ?)";
     private static final String DELETE_QUERY = "DELETE FROM likes WHERE user_id = ? AND film_id = ?";
-    private static final String FIND_POPULAR_FILMS_QUERY = """
-            SELECT f.id, f.name, f.description, f.release_date, f.duration, f.rating_id, COUNT(fl.user_id) AS likes
-            FROM films f LEFT JOIN likes fl ON f.id = fl.film_id GROUP BY f.id ORDER BY likes DESC, f.id ASC LIMIT ?""";
 
     private static final String FIND_USERS_COMMON_FILMS_QUERY = """
             SELECT f.id, f.name, f.description, f.release_date, f.duration, f.rating_id
@@ -43,11 +40,6 @@ public class LikeRepository extends BaseRepository<Like> {
     public void deleteLike(Long userId, Long filmId) {
         log.debug("Запрос удаления лайка пользователя (Id: {}) с фильма (Id: {})", userId, filmId);
         jdbc.update(DELETE_QUERY, userId, filmId);
-    }
-
-    public Collection<Film> findPopularFilms(int count) {
-        log.debug("Запрос на список {} популярных фильмов", count);
-        return jdbc.query(FIND_POPULAR_FILMS_QUERY, new FilmRowMapper(), count);
     }
 
     public Collection<Film> findUsersCommonFilms(Long userId, Long friendId) {
